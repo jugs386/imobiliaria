a99f564603fad7efb1ce297b7efcb7c76f3c015f
Juliana de Sá <julianagsa@yahoo.com.br>
2020-11-30 22:48:08 -0300

    banco de dados

---
+++ imobiliaria_alterado.sql
@@ -0,0 +1,28 @@
+create database imobiliaria;
+use imobiliaria;
+
+create table usuario(
+	id int(4) auto_increment primary key,
+    login varchar(10),
+    senha varchar(100),
+    permissao char(1)
+);
+
+create table imovel(
+	id int(4) auto_increment primary key,
+    descricao text,
+    foto longblob,
+    fotoTipo varchar(20),
+    valor decimal(9,2),
+    tipo char(1)
+);
+
+create table visualizacao(
+	id int(4) auto_increment primary key,
+    idImovel int (4) not null,
+    data date,
+    hora time,
+    foreign key(idImovel) references Imovel(id)
+);
+
+insert into usuario values (null,'admin','e10adc3949ba59abbe56e057f20f883e','A');
